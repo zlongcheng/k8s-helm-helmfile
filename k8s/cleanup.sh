3edc8174a7cf986455a602ed01dd5c6b58ff0461
Long Cheng <lcheng@netspend.com>
2024-07-01 13:41:50 -0500

    save

---
+++ k8s/cleanup.sh
@@ -0,0 +1,16 @@
+# delete ingress
+kubectl delete ingresses.networking.k8s.io ingress-service
+# delete services
+kubectl delete svc adminer   
+kubectl delete svc kanban-app
+kubectl delete svc kanban-ui 
+kubectl delete svc postgres  
+# delete deployments
+kubectl delete deployments.apps adminer    
+kubectl delete deployments.apps kanban-app 
+kubectl delete deployments.apps kanban-ui  
+kubectl delete deployments.apps postgres   
+# delete configmaps
+kubectl delete configmaps postgres-config
+# delete persistent volume claim
+kubectl delete pvc postgres-persistent-volume-claim
